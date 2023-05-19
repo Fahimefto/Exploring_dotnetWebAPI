008d57f6dede9a27296878c478f399d9d9771ff5
Fahim Iftekhar <fahim.iftekhar.efto@gmail.com>
2023-05-19 19:14:57 +0600

    initialize

---
+++ CrudAPI/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using CrudAPI.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/contact")]
+    public class ContactController : Controller
+    {
+        private readonly ContactsAPIdbContext dbContext;
+
+        public ContactController( ContactsAPIdbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllContact()
+        {
+            return Ok(dbContext.Contacts.ToList());
+
+        }
+    }
+}
---
+++ CrudAPI/Data/ContactsAPIdbContext.cs
@@ -0,0 +1,13 @@
+﻿using CrudAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAPI.Data
+{
+    public class ContactsAPIdbContext : DbContext
+    {
+        public ContactsAPIdbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
